799296e05743c8d1475baebd493e46c695e6a5df
Adunda13 <64tokyo@gmail.com>
2019-10-12 12:42:17 -0700

    Completed Do Vue in about 60 minutes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <Skills />
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+
+    </nav>
+
+    <router-view />
   </div>
 </template>
 
@@ -16,12 +22,39 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top:50px;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    width: 50%;
+}
+
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #c24024;
+  border-radius: 3px;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
+nav a:hover {
+  background:rgb(255, 255, 255);
+  color:#c24024;
 }
 </style>
